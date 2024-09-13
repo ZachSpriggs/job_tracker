b789a432a79abb274b5a353117b7d0290e679cec
zachspriggs <zachspriggs@gmail.com>
2024-09-12 20:18:34 -0700

    added a few things to the frontend

---
+++ frontend/src/components/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/homepage.css"
+
+const homepage = () => {
+  return (
+    <div className="container">
+      <div>
+        Here is the main portion of my website. I'm going to need to add the kanban below
+      </div>
+    </div>
+  )
+}
+
+export default homepage
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <nav>
+        here is all my navbar
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
